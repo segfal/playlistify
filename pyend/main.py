3bda7101851999b9eb233170b1fc14a18088e910
segfal <segfaultboi@gmail.com>
2023-12-02 22:17:08 -0500

    Added a README.md file and made a flask main.py

--- pyend/main.py
+++ pyend/main.py
@@ -1,60 +1,19 @@
-#dotenv
-import os,sys,subprocess,requests,json,time
-from dotenv import load_dotenv
-
-load_dotenv()
+from flask import Flask #Flask is used to create the server
+from flask_cors import CORS #Cors is used to allow cross origin requests
 
 
+app = Flask(__name__) #Creates the server
+CORS(app) #Allows cross origin requests
 
-def json_print(json_data,filename):
-    with open(filename,"w") as file:
-        json.dump(json_data,file,indent=4)
+@app.route("/") #Creates a route for the server
+def hello_world():
+    return "Hello World!" #Returns the string "Hello World!"
 
-
-CREDENTIALS = {
-    "CLIENT_ID" : os.getenv("CLIENT_ID"),
-    "CLIENT_SECRET" : os.getenv("CLIENT_SECRET")
-}
-
-
-def get_token():
-    url = "https://accounts.spotify.com/api/token"
-    payload = {
-        "grant_type" : "client_credentials"
-    }
-    response = requests.post(url, data=payload, auth=(CREDENTIALS["CLIENT_ID"], CREDENTIALS["CLIENT_SECRET"]))
-    return response.json()["access_token"]
+@app.route("/user/<name>") #Creates a route for the server
+def hello_name(name):
+    return f"Hello {name}!"
 
 
 
-# find a song
-def find_song(song_name):
-    url = "https://api.spotify.com/v1/search"
-    params = {
-        "q" : song_name,
-        "type" : "track",
-        "limit" : 1
-    }
-    headers = {
-        "Authorization" : f"Bearer {get_token()}"
-    }
-    response = requests.get(url, params=params, headers=headers)
-    return response.json()["tracks"]["items"][0]["id"]
-
-
-
-def get_track(song_name = None ,song_id = None):
-    song = song_name if song_name else song_id
-    #song = find_song(song)
-    url = f"https://api.spotify.com/v1/tracks/{song}"
-    headers = {
-        "Authorization" : f"Bearer {get_token()}"
-    }
-    response = requests.get(url, headers=headers)
-    return response.json()
-
-
-json_print(get_track(song_id="11dFghVXANMlKmJXsNCbNl"),"hello.json")
-
-
+app.run()
 
