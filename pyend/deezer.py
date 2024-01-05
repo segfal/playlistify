5defbc0df08b0d5cdacbe911f5f71ceb29ad3cb3
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2024-01-05 00:46:34 -0400

    implement login flow with Deezer Oauth to get access token, and store it on session

--- pyend/deezer.py
+++ pyend/deezer.py
@@ -1,4 +1,11 @@
 import requests
+from flask import redirect, session
+import os
+from dotenv import load_dotenv, find_dotenv
+
+dotenv_path = find_dotenv()
+
+load_dotenv(dotenv_path)
 
 '''
 Its better to use the requests library to make the api calls instead of the deezer python library
@@ -7,9 +14,23 @@ because its more flexible to use requests library, its like axios in javascript
 
 
 class Deezer:
+    
+
+    #login
+    def login():
+        url = (f"https://connect.deezer.com/oauth/auth.php?app_id={os.getenv("DEZEER_APP_ID")}&redirect_uri={os.getenv("DEZEER_REDIRET_URI")}&perms=basic_access,email,manage_library,listening_history,offline_access")
+        return redirect(url)
+    
+    #get access_token
+    def get_access_token(code):
+        url = (f"https://connect.deezer.com/oauth/access_token.php?app_id={os.getenv("DEZEER_APP_ID")}&secret={os.getenv("DEZEER_SECRET_KEY")}&code={code}&output=json")
+        response = requests.get(url)
+        return response.json();
+        
+    
     #get genre
-    def get_genre(self,genre):
-        url = "https://api.deezer.com/genre/"+genre
+    def get_genre(self, access_token):
+        url = f"https://api.deezer.com/genre"
         response = requests.get(url)
         return response.json()
     
@@ -42,8 +63,14 @@ only edit the Deezer class
 I only encapsulated the class in a function because its easier to import the functions from deezer.py
 
 '''
-def get_genre(genre):
-    return Deezer().get_genre(genre)
+def login():
+    return Deezer.login()
+
+def get_access_token(code):
+    return Deezer.get_access_token(code)
+
+def get_genre(access_token):
+    return Deezer().get_genre(access_token=access_token)
 
 def get_artist(artist):
     return Deezer().get_artist(artist)
--- pyend/main.py
+++ pyend/main.py
@@ -1,37 +1,62 @@
-from flask import Flask #Flask is used to create the server
-from flask_cors import CORS #Cors is used to allow cross origin requests
-from deezer import get_genre,get_album,get_artist,get_track #Import the functions from deezer.py
+from flask import Flask, request, session  # Flask is used to create the server
+from flask_cors import CORS  # Cors is used to allow cross origin requests
+from deezer import (
+    get_genre,
+    get_album,
+    get_artist,
+    get_track,
+    login,
+    get_access_token,
+)  # Import the functions from deezer.py
+import os
 
-app = Flask(__name__) #Creates the server
-CORS(app) #Allows cross origin requests
+app = Flask(__name__)  # Creates the server
+CORS(app)  # Allows cross origin requests
+app.secret_key = os.getenv("SECRET_KEY")
 
-@app.route("/") #Creates a route for the server
+
+@app.route("/")  # Creates a route for the server
 def hello_world():
-    return "Hello World!" #Returns the string "Hello World!"
+    return "Hello World!"  # Returns the string "Hello World!"
 
-@app.route("/user/<name>") #Creates a route for the server
+
+@app.route("/user/<name>")  # Creates a route for the server
 def hello_name(name):
     return f"Hello {name}!"
 
 
-#Routes for the deezer api
-@app.route("/deezer/genre/<genre>")
-def deezer_genre(genre):
-    return get_genre(genre)
+# Routes for the deezer api
+@app.route("/deezer/login/")
+def deezer_login():
+    return login()
+
+
+@app.route("/deezer/redirect")
+def deezer_redirect():
+    code = request.args["code"]
+    access_token = get_access_token(code)["access_token"]
+    session["access_token"] = access_token
+    return "Access_Token Retrive Successfully"
+
+
+@app.route("/deezer/genre")
+def deezer_genre():
+    return get_genre(access_token=session.get("access_token"))
+
 
 @app.route("/deezer/artist/<artist>")
 def deezer_artist(artist):
     return get_artist(artist)
 
+
 @app.route("/deezer/album/<album>")
 def deezer_album(album):
     return get_album(album)
+
 
 @app.route("/deezer/track/<track>")
 def deezer_track(track):
     return get_track(track)
 
 
-
 app.run()
-
