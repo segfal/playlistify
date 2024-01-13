6f94f97a7b4a789ccb8907703d77ec9c5a9dc9ef
segfal <segfaultboi@gmail.com>
2024-01-13 14:32:34 -0500

    data modeling

--- pyend/json_data/read_json.py
+++ pyend/json_data/read_json.py
@@ -1,7 +1,25 @@
 import json
+import os
 
 
-with open("country.json", "r") as file:
-    data = json.load(file)
+json_files = os.listdir()
+json_files = [file for file in json_files if file.endswith('.json')]
 
-print(len(data))
+
+lens = 0
+table = []
+# remove country.json
+json_files.remove('country.json')
+for file in json_files:
+    with open(file, 'r') as f:
+        data = json.load(f)
+        lens += len(data[1])
+        table.append(data[1])
+
+#print(table[0])
+x = len(table[0])
+
+num = 0
+for i in table:
+    num += len(i)
+print(num)
