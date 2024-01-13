eb9ea153f417066738987b205613e83a8a042c2a
segfal <segfaultboi@gmail.com>
2024-01-13 16:42:56 -0500

    playlists

--- pyend/json_data/read_json.py
+++ pyend/json_data/read_json.py
@@ -2,24 +2,7 @@ import json
 import os
 
 
-json_files = os.listdir()
-json_files = [file for file in json_files if file.endswith('.json')]
+json_files = os.listdir('./json_files')
 
 
-lens = 0
-table = []
-# remove country.json
-json_files.remove('country.json')
-for file in json_files:
-    with open(file, 'r') as f:
-        data = json.load(f)
-        lens += len(data[1])
-        table.append(data[1])
-
-#print(table[0])
-x = len(table[0])
-
-num = 0
-for i in table:
-    num += len(i)
-print(num)
+print(json_files)
